e1f36a0d9ecba1a446d4f6f08a2db733beef15db
Javier <javierelcachas@gmail.com>
2021-08-23 18:58:55 +0200

    All fusion ChartController API

--- app/Http/Controllers/Api/ChartController.php
+++ app/Http/Controllers/Api/ChartController.php
@@ -144,4 +144,92 @@ class ChartController extends Controller
        return $arrayValue;
     }
 
+    public function setPointStart($arrayData){
+
+        foreach($arrayData as $key => $data){
+            if($data != -1){
+                return $key;
+            }
+        }
+        return 0;
+    }
+
+    public function getDataChart()
+    {
+        $user = Auth::user();
+        $goals_weight = Goal::where('user_id' , $user->id)->where('name_goal_id' , 1)->first();
+        $goals_body_fat = Goal::where('user_id' , $user->id)->where('name_goal_id' , 2)->first();
+        $measurements = Body::where('user_id' , $user->id)->orderBy('date' , 'DESC')->get();
+
+        $mouths = Body::where('user_id' , $user->id)->orderBy('date')->get();
+        $countMouths = [];
+        $countMouthsFormat = [];
+        $goalWeightCount = [];
+        $goalBodyFatCount = [];
+
+
+       
+
+        $goals_weight_array = Goal::where('user_id' , $user->id)->where('name_goal_id' , 1)->get();
+        $goals_body_fat_array = Goal::where('user_id' , $user->id)->where('name_goal_id' , 2)->get();
+
+        foreach($mouths as $mouth){
+            array_push($countMouths , $mouth->date_format);
+        }
+
+         $countMouths = array_unique($countMouths);
+         $countMouths = array_values($countMouths);
+
+
+         if($goals_weight_array){
+            $goals_weight_array = $goals_weight_array->first();
+            for($i = 0 ; $i< count($countMouths) ; $i++){
+                array_push($goalWeightCount , $goals_weight_array['value']);
+            }
+         }
+
+         if($goals_body_fat_array){
+            $goals_body_fat_array = $goals_body_fat_array->first();
+            for($i = 0 ; $i< count($countMouths) ; $i++){
+                array_push($goalBodyFatCount , $goals_body_fat_array['value']);
+            }
+         }
+
+        $measurementsWeight = Body::select('value' , 'date')->where('user_id' , $user->id)->where('stat_id' , 1)->orderBy('date')->get()->toArray();
+        $measurementsBodyFat = Body::select('value' , 'date')->where('user_id' , $user->id)->where('stat_id' , 2)->orderBy('date')->get()->toArray();
+
+     //  dd($measurementsWeight);
+
+        $arrayWeight = $this->createArrayData($countMouths, $measurementsWeight);
+        $pointStartWeight = $this->setPointStart($arrayWeight);
+
+        $arrayBodyFat = $this->createArrayData($countMouths, $measurementsBodyFat);
+        $pointStartBodyFat = $this->setPointStart($arrayBodyFat);
+
+        foreach($arrayWeight as $key =>$weight){
+            if($weight == -1){
+                unset($arrayWeight[$key]);
+            }
+        }
+
+        foreach($arrayBodyFat as $key =>$body){
+            if($body == -1){
+                unset($arrayBodyFat[$key]);
+            }
+        }
+
+        $arrayWeight = array_values($arrayWeight);
+        $arrayBodyFat = array_values($arrayBodyFat);
+
+        return compact(     'countMouths' , 
+                            'goalWeightCount' , 
+                            'goalBodyFatCount',
+                            'arrayWeight',
+                            'pointStartWeight',
+                            'arrayBodyFat',
+                            'pointStartBodyFat'
+                    );
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,7 @@ Route::middleware('auth:api')->group(function () {
     Route::get('goal/body' , 'ChartController@getGoalBodyFat');
     Route::get('weight' , 'ChartController@getWeight');
     Route::get('body' , 'ChartController@getBodyFat');
+    Route::get('data' , 'ChartController@getDataChart');
 
 });
 
